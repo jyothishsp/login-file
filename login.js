6e07ac979d5661767ad5de1faf56e07f98016960
Jyothih S P <jyothishsp1993@gmail.com>
2018-10-18 14:14:25 +0530

    dummy Login program full

--- login.js
+++ login.js
@@ -1,8 +1,24 @@
 var express=require('express')
-var app =express
-app.get('',function(req,res){
-  console.console.log("Enterin into the login page..");
+var app =express()
+var path=require('path')
+
+app.get('/',function(req,res){
+  console.log("Entering into the login page..");
   res.sendFile(path.join(__dirname+'/index.html'))
+})
+app.get('/login_process', function (req, res) {
+   response = {
+      mail:req.query.mail,
+      password:req.query.password
+   }
+   console.log(res);
+   //res.send(JSON.stringify(res));
+   if(req.query.mail=="demo@demo.com" && req.query.password=="12345678"){
+     res.send("log in sucessfully done..");
+   }
+   else {
+     res.send("Invalid entry..");
+   }
 })
 const server=app.listen("8000","localhost",function(){
 	console.log("Server running...")
